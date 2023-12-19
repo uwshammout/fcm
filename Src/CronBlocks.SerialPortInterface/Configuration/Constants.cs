695ae1996edbdd971b7ad33c16cc48d0de1d1c91
usama <mahmoodify@gmail.com>
2023-12-19 21:21:27 +0500

    adding endianess const

--- Src/CronBlocks.SerialPortInterface/Configuration/Constants.cs
+++ Src/CronBlocks.SerialPortInterface/Configuration/Constants.cs
@@ -1,8 +1,13 @@
-﻿namespace CronBlocks.SerialPortInterface.Configuration;
+﻿using FluentModbus;
+
+namespace CronBlocks.SerialPortInterface.Configuration;
 
 internal class Constants
 {
     // Time Constants
     public static readonly double PortDiscoveryIntervalMS = 100;
     public static readonly double DataAcquisitionIntervalMS = 250;
+
+    // Endianness
+    public static readonly ModbusEndianness ModbusEndianness = ModbusEndianness.BigEndian;
 }
