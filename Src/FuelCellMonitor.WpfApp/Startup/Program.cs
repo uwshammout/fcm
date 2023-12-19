4772ab6190213c75989c3b8058babc95b42925c5
usama <mahmoodify@gmail.com>
2023-12-19 17:31:55 +0500

    adding initial app with a bit of refactoring

---
+++ Src/FuelCellMonitor.WpfApp/Startup/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace CronBlocks.FuelCellMonitor.Startup;
+
+public class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        App app = new App();
+        app.InitializeComponent();
+        app.Run();
+    }
+}
---
+++ Src/FuelCellMonitor.WpfApp/Windows/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows;
+
+namespace CronBlocks.FuelCellMonitor.Windows;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
