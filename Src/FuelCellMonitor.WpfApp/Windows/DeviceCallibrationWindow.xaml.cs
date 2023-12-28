b6f2d505704577f2b98f931dc6a267893eca4e7d
usama <mahmoodify@gmail.com>
2023-12-28 18:53:20 +0500

    adding device callibration window

---
+++ Src/FuelCellMonitor.WpfApp/Windows/DeviceCallibrationWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using CronBlocks.SerialPortInterface.Interfaces;
+using System.Windows;
+
+namespace CronBlocks.FuelCellMonitor.Windows;
+
+public partial class DeviceCallibrationWindow : Window
+{
+    private readonly ISerialModbusClientService _modbus;
+    private readonly ISerialModbusDataScalingService _modbusScaling;
+
+    public DeviceCallibrationWindow(
+        ISerialModbusClientService modbus,
+        ISerialModbusDataScalingService modbusScaling)
+    {
+        InitializeComponent();
+
+        _modbus = modbus;
+        _modbusScaling = modbusScaling;
+    }
+}
