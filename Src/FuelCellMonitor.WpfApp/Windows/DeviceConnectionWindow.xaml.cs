96f11c4cc1798b2c4f4264c570c3dfb111e15a02
usama <mahmoodify@gmail.com>
2023-12-28 17:57:37 +0500

    adding device connection window

---
+++ Src/FuelCellMonitor.WpfApp/Windows/DeviceConnectionWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using CronBlocks.SerialPortInterface.Interfaces;
+using System.Windows;
+
+namespace CronBlocks.FuelCellMonitor.Windows;
+
+public partial class DeviceConnectionWindow : Window
+{
+    private readonly ISerialPortsDiscoveryService _serialPortsDiscovery;
+    private readonly ISerialOptionsService _serialOptions;
+    private readonly ISerialModbusClientService _serialModbusClient;
+
+    public DeviceConnectionWindow(
+        ISerialPortsDiscoveryService serialPortsDiscovery,
+        ISerialOptionsService serialOptions,
+        ISerialModbusClientService serialModbusClient)
+    {
+        InitializeComponent();
+
+        _serialPortsDiscovery = serialPortsDiscovery;
+        _serialOptions = serialOptions;
+        _serialModbusClient = serialModbusClient;
+    }
+}
