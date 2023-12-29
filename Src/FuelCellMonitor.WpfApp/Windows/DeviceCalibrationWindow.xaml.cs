523a8acfd777bf2e713a5a4699acb0cc61b3202f
usama <mahmoodify@gmail.com>
2023-12-30 02:54:27 +0500

    outlining the main UI

--- Src/FuelCellMonitor.WpfApp/Windows/DeviceCalibrationWindow.xaml.cs
+++ Src/FuelCellMonitor.WpfApp/Windows/DeviceCalibrationWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using CronBlocks.SerialPortInterface.Interfaces;
+﻿using CronBlocks.SerialPortInterface.Entities;
+using CronBlocks.SerialPortInterface.Interfaces;
 using System.Windows;
 
 namespace CronBlocks.FuelCellMonitor.Windows;
@@ -16,5 +17,47 @@ public partial class DeviceCalibrationWindow : Window
 
         _modbus = modbus;
         _modbusScaling = modbusScaling;
+
+        _modbus.OperationStateChanged += OnDeviceOperationStateChanged;
+        OnDeviceOperationStateChanged(_modbus.OperationState);
+
+        _modbusScaling.NewValuesReceived += OnNewValuesReceived;
+    }
+
+    private void OnNewValuesReceived(List<double> list)
+    {
+    }
+
+    private void OnDeviceOperationStateChanged(OperationState state)
+    {
+        Dispatcher.Invoke(() =>
+        {
+            switch (state)
+            {
+                case OperationState.Running:
+                    StatusMessage.Text =
+                        "Device connected -" +
+                        " Dynamic updates available";
+                    break;
+
+                case OperationState.Stopped:
+                    StatusMessage.Text =
+                        "Device not connected -" +
+                        " dynamic updates not available";
+
+                    break;
+            }
+        });
+    }
+
+    private void OnTextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+    {
+
+    }
+
+    protected override void OnClosed(EventArgs e)
+    {
+        _modbusScaling.SaveValuesToFile();
+        base.OnClosed(e);
     }
 }
