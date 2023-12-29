fd80330ba3f038af717cb0bfe7c7c4b04978e17a
usama <mahmoodify@gmail.com>
2023-12-29 23:16:35 +0500

    adding error color

---
+++ Src/FuelCellMonitor.WpfApp/Settings/DisplayColors.cs
@@ -0,0 +1,8 @@
+﻿using System.Windows.Media;
+
+namespace CronBlocks.FuelCellMonitor.Settings;
+
+internal static class DisplayColors
+{
+    public readonly static SolidColorBrush ErrorBg = new(Color.FromArgb(200, 150, 50, 55));
+}
