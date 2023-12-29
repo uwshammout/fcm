8b8699d741c3258f972c4cf186c673d905697052
usama <mahmoodify@gmail.com>
2023-12-29 21:24:20 +0500

    displaying discovered ports

--- Src/FuelCellMonitor.WpfApp/Windows/DeviceConnectionWindow.xaml.cs
+++ Src/FuelCellMonitor.WpfApp/Windows/DeviceConnectionWindow.xaml.cs
@@ -10,12 +10,32 @@ public partial class DeviceConnectionWindow : Window
     private readonly ISerialOptionsService _options;
     private readonly ISerialModbusClientService _modbus;
 
+    private readonly List<FrameworkElement> _allSelectionElements;
+
     public DeviceConnectionWindow(
         ISerialPortsDiscoveryService serialPortsDiscovery,
         ISerialOptionsService serialOptions,
         ISerialModbusClientService serialModbusClient)
     {
         InitializeComponent();
+
+        _allSelectionElements = new List<FrameworkElement>()
+        {
+            PortHeading,
+            BaudRateHeading,
+            DataBitsHeading,
+            ParityHeading,
+            StopBitsHeading,
+            DeviceAddressHeading,
+            RegistersStartAddressHeading,
+            PortInput,
+            BaudRateInput,
+            DataBitsInput,
+            ParityInput,
+            StopBitsInput,
+            DeviceAddressInput,
+            RegistersStartAddressInput,
+        };
 
         _ports = serialPortsDiscovery;
         _options = serialOptions;
@@ -38,25 +58,65 @@ public partial class DeviceConnectionWindow : Window
 
     private void OnModbusStatusChanged(OperationState status)
     {
-        switch (status)
+        Dispatcher.Invoke(() =>
         {
-            case OperationState.Running:
-                break;
+            switch (status)
+            {
+                case OperationState.Running:
+                    _ports.NewPortFound -= OnComPortFound;
+                    _ports.ExistingPortRemoved -= OnComPortRemoved;
+                    _ports.StopPortsDiscovery();
 
-            case OperationState.Stopped:
-                _ports.NewPortFound += OnComPortFound;
-                _ports.ExistingPortRemoved += OnComPortRemoved;
-                _ports.StartPortsDiscovery();
-                break;
+                    EnableSelectionControls(false);
+                    break;
+
+                case OperationState.Stopped:
+                    _ports.NewPortFound += OnComPortFound;
+                    _ports.ExistingPortRemoved += OnComPortRemoved;
+                    _ports.StartPortsDiscovery();
+
+                    EnableSelectionControls(true);
+                    break;
+            }
+        });
+    }
+
+    private void EnableSelectionControls(bool enable)
+    {
+        foreach (var item in _allSelectionElements)
+        {
+            item.IsEnabled = enable;
         }
     }
 
     private void OnComPortFound(string port)
     {
+        if (_modbus.OperationState == OperationState.Running)
+            return;
+
+        Dispatcher.Invoke(() =>
+        {
+            if (PortInput.Items.Contains(port)) return;
+
+            PortInput.Items.Add(port);
+
+            if (PortInput.Items.Count == 1)
+            {
+                PortInput.SelectedIndex = 0;
+            }
+        });
     }
 
     private void OnComPortRemoved(string port)
     {
+        if (_modbus.OperationState == OperationState.Running)
+            return;
+
+        Dispatcher.Invoke(() =>
+        {
+            if (PortInput.Items.Contains(port))
+                PortInput.Items.Remove(port);
+        });
     }
 
     private void ConnectButtonClicked(object sender, RoutedEventArgs e)
