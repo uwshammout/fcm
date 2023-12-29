b994dc7fee184773de95704054533a86eb527b19
usama <mahmoodify@gmail.com>
2023-12-29 18:06:23 +0500

    outlining

--- Src/FuelCellMonitor.WpfApp/Windows/DeviceConnectionWindow.xaml.cs
+++ Src/FuelCellMonitor.WpfApp/Windows/DeviceConnectionWindow.xaml.cs
@@ -1,13 +1,14 @@
-﻿using CronBlocks.SerialPortInterface.Interfaces;
+﻿using CronBlocks.SerialPortInterface.Entities;
+using CronBlocks.SerialPortInterface.Interfaces;
 using System.Windows;
 
 namespace CronBlocks.FuelCellMonitor.Windows;
 
 public partial class DeviceConnectionWindow : Window
 {
-    private readonly ISerialPortsDiscoveryService _serialPortsDiscovery;
-    private readonly ISerialOptionsService _serialOptions;
-    private readonly ISerialModbusClientService _serialModbusClient;
+    private readonly ISerialPortsDiscoveryService _ports;
+    private readonly ISerialOptionsService _options;
+    private readonly ISerialModbusClientService _modbus;
 
     public DeviceConnectionWindow(
         ISerialPortsDiscoveryService serialPortsDiscovery,
@@ -16,17 +17,46 @@ public partial class DeviceConnectionWindow : Window
     {
         InitializeComponent();
 
-        _serialPortsDiscovery = serialPortsDiscovery;
-        _serialOptions = serialOptions;
-        _serialModbusClient = serialModbusClient;
+        _ports = serialPortsDiscovery;
+        _options = serialOptions;
+        _modbus = serialModbusClient;
 
-        _serialPortsDiscovery.StartPortsDiscovery();
+        _modbus.OperationStateChanged += OnModbusStatusChanged;
+        OnModbusStatusChanged(_modbus.OperationState);
     }
 
     protected override void OnClosed(EventArgs e)
     {
-        _serialPortsDiscovery.StopPortsDiscovery();
+        _modbus.OperationStateChanged -= OnModbusStatusChanged;
+
+        _ports.NewPortFound -= OnComPortFound;
+        _ports.ExistingPortRemoved -= OnComPortRemoved;
+        _ports.StopPortsDiscovery();
+
         base.OnClosed(e);
+    }
+
+    private void OnModbusStatusChanged(OperationState status)
+    {
+        switch (status)
+        {
+            case OperationState.Running:
+                break;
+
+            case OperationState.Stopped:
+                _ports.NewPortFound += OnComPortFound;
+                _ports.ExistingPortRemoved += OnComPortRemoved;
+                _ports.StartPortsDiscovery();
+                break;
+        }
+    }
+
+    private void OnComPortFound(string port)
+    {
+    }
+
+    private void OnComPortRemoved(string port)
+    {
     }
 
     private void ConnectButtonClicked(object sender, RoutedEventArgs e)
