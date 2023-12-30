606efb1643ab793f9d486e93911d0bff39fa17bf
usama <mahmoodify@gmail.com>
2023-12-30 05:10:49 +0500

    exposing constants to outer world

--- Src/CronBlocks.SerialPortInterface/Configuration/Constants.cs
+++ Src/CronBlocks.SerialPortInterface/Configuration/Constants.cs
@@ -3,16 +3,16 @@ using FluentModbus;
 
 namespace CronBlocks.SerialPortInterface.Configuration;
 
-internal class Constants
+public class Constants
 {
     // Time Constants
-    public const double PortDiscoveryIntervalMS = 100;
+    internal const double PortDiscoveryIntervalMS = 100;
     public const double DefaultDataAcquisitionIntervalMS = 500;
     public const double MinimumDataAcquisitionIntervalMS = 250;
     public const double MaximumDataAcquisitionIntervalMS = 1500;
 
     // Device Constants
-    public const ModbusEndianness ModbusEndianness = FluentModbus.ModbusEndianness.BigEndian;
+    internal const ModbusEndianness ModbusEndianness = FluentModbus.ModbusEndianness.BigEndian;
     public const int TotalRegisters = 16;
 
     // Defaults
