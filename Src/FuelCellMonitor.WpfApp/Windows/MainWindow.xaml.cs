e55d34189c8293bb4c7d1d0bea0f46e38599a2ab
usama <mahmoodify@gmail.com>
2023-12-30 05:11:30 +0500

    updating gauge values

--- Src/FuelCellMonitor.WpfApp/Windows/MainWindow.xaml.cs
+++ Src/FuelCellMonitor.WpfApp/Windows/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ public partial class MainWindow : Window
         ISerialModbusDataScalingService scalingService)
     {
         InitializeComponent();
-        
+
         _app = app;
         _modbus = modbus;
         _modbusScaling = scalingService;
@@ -39,6 +39,44 @@ public partial class MainWindow : Window
 
     private void OnNewValuesReceived(List<double> values)
     {
+        if (values.Count < CronBlocks.SerialPortInterface.Configuration.Constants.TotalRegisters)
+        {
+            throw new InvalidOperationException(
+                $"Expected number of values is {CronBlocks.SerialPortInterface.Configuration.Constants.TotalRegisters}" +
+                $" whereas, {values.Count} values are received");
+        }
+
+        Dispatcher.Invoke(() =>
+        {
+            //- Fuel Cell
+
+            double fcTotalVoltage = values[0];
+            double fcTotalCurrent = values[1];
+            double fcTotalPower = fcTotalVoltage * fcTotalCurrent;
+            double fcC1Voltage = values[2];  //- First cell near ground
+            double fcC2Voltage = values[3];  //- 
+            double fcC3Voltage = values[4];  //- 
+            double fcC4Voltage = values[5];  //- 
+            double fcC5Voltage = values[6];  //- 
+            double fcC6Voltage = values[7];  //- 
+            double fcC7Voltage = values[8];  //- 
+            double fcC8Voltage = values[9];  //- 
+            double fcC9Voltage = values[10]; //- Last cell near the total voltage
+
+            FuelCellVoltageGauge.Dial1Value = fcTotalVoltage;
+            FuelCellCurrentGauge.Dial1Value = fcTotalCurrent;
+            FuelCellPowerGauge.Dial1Value = fcTotalPower;
+
+            //- Electrolyzer
+
+            double elTotalVoltage = values[11];
+            double elTotalCurrent = values[12];
+            double elTotalPower = elTotalVoltage * elTotalCurrent;
+
+            ElectrolyzerVoltageGauge.Dial1Value = elTotalVoltage;
+            ElectrolyzerCurrentGauge.Dial1Value = elTotalCurrent;
+            ElectrolyzerPowerGauge.Dial1Value = elTotalPower;
+        });
     }
 
     private void OnDeviceOperationStateChanged(OperationState state)
@@ -72,7 +110,7 @@ public partial class MainWindow : Window
                 switch (header)
                 {
                     case "Connect / Disconnect":
-                        window = _app.GetInstance<DeviceConnectionWindow>();                        
+                        window = _app.GetInstance<DeviceConnectionWindow>();
                         break;
 
                     case "Calibrate":
@@ -86,6 +124,6 @@ public partial class MainWindow : Window
                 }
             }
         }
-        
+
     }
 }
