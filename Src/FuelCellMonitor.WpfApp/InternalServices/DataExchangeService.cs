8fb4f49249d16b728bb0e34c5d2709583f5348dc
usama <mahmoodify@gmail.com>
2023-12-31 05:38:40 +0500

    adding DataExchangeService for inter-window comm

---
+++ Src/FuelCellMonitor.WpfApp/InternalServices/DataExchangeService.cs
@@ -0,0 +1,41 @@
+﻿using CronBlocks.FuelCellMonitor.Settings;
+using CronBlocks.Helpers;
+
+namespace CronBlocks.FuelCellMonitor.InternalServices;
+
+public class DataExchangeService : IDisposable
+{
+    private readonly IniConfigIO _iniConfig;
+
+    public double ExperimentTimeDuration { get; set; }
+
+    public DataExchangeService(IniConfigIO iniConfig)
+    {
+        _iniConfig = iniConfig;
+
+        LoadConfig();
+    }
+
+    private void LoadConfig()
+    {
+        ExperimentTimeDuration = _iniConfig.GetDouble(KeyOf(nameof(ExperimentTimeDuration)), ValueConstants.ExperimentTimeMinimumSec + 20);
+    }
+
+    private void SaveConfig()
+    {
+        _iniConfig.SetDouble(KeyOf(nameof(ExperimentTimeDuration)), ExperimentTimeDuration);
+
+        _iniConfig.SaveFile();
+    }
+
+    private string KeyOf(string paramName)
+    {
+        const string sectionName = nameof(DataExchangeService);
+        return $"{sectionName}/{paramName}";
+    }
+
+    public void Dispose()
+    {
+        SaveConfig();
+    }
+}
