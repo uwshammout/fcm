bb4e384fb6eb094382f876337a436e005df98590
usama <mahmoodify@gmail.com>
2023-12-31 05:40:56 +0500

    adding value constants

---
+++ Src/FuelCellMonitor.WpfApp/Settings/ValueConstants.cs
@@ -0,0 +1,7 @@
+﻿namespace CronBlocks.FuelCellMonitor.Settings;
+
+internal static class ValueConstants
+{
+    public readonly static double ExperimentTimeMinimumSec = 5;
+    public readonly static double ExperimentTimeMaximumSec = 500;
+}
