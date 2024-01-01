f9aa2b4dbd951822864e05324b636ef745bc2865
usama <mahmoodify@gmail.com>
2024-01-02 01:23:04 +0500

    cleanup

--- Src/CronBlocks.UserControls.Wpf/XYPlot/XYPlot.xaml.cs
+++ Src/CronBlocks.UserControls.Wpf/XYPlot/XYPlot.xaml.cs
@@ -1,22 +1,9 @@
-﻿using CronBlocks.UserControls.Wpf.TimedPlot;
+﻿using LiveCharts;
 using LiveCharts.Configurations;
-using LiveCharts;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Runtime.CompilerServices;
 
 namespace CronBlocks.UserControls.Wpf.XYPlot;
 
