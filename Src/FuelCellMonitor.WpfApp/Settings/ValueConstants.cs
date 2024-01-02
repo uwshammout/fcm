c668b84447400f62c100ee10517f0cef69a361f6
usama <mahmoodify@gmail.com>
2024-01-02 22:14:25 +0500

    adding default values for resistors

--- Src/FuelCellMonitor.WpfApp/Settings/ValueConstants.cs
+++ Src/FuelCellMonitor.WpfApp/Settings/ValueConstants.cs
@@ -4,4 +4,7 @@ internal static class ValueConstants
 {
     public readonly static double ExperimentTimeMinimumSec = 5;
     public readonly static double ExperimentTimeMaximumSec = 500;
+
+    public readonly static double FuelCellCurrentMeasurementResistanceOhm = 1;
+    public readonly static double ElectrolyzerCurrentMeasurementResistanceOhm = 1;
 }
