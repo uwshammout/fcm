d7a694dfa94a49584f0013d5ccfe58180f599019
usama <mahmoodify@gmail.com>
2024-01-04 04:43:47 +0500

    offsetting and scaling

--- Src/ArduinoSimDevice/SimpleGen.py
+++ Src/ArduinoSimDevice/SimpleGen.py
@@ -1,9 +1,18 @@
 from math import sin, cos, pi
 
 offset = 1.0
+scale = 128
 
 for deg in range(0,360):
     rad = deg * pi / 180
-    sine = sin(rad) + offset
-    cosine = cos(rad) + offset
-    print(f"{deg}, {rad} => {sine}, {cosine}")
+    
+    sine = sin(rad)
+    cosine = cos(rad)
+    
+    offset_sine = sine + offset
+    offset_cosine = cosine + offset
+    
+    offset_scaled_sine = int(offset_sine * scale)
+    offset_scaled_cosine = int(offset_cosine * scale)
+
+    print(f"{deg}, {rad} => {offset_scaled_sine}, {offset_scaled_cosine}")
