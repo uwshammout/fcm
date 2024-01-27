93ae77d13bfcf02dadb74ccd5397ba48f51900ff
usama <mahmoodify@gmail.com>
2024-01-27 18:19:44 +0500

    simplifying

--- Src/ArduinoSimDevice/SimData.cpp
+++ Src/ArduinoSimDevice/SimData.cpp
@@ -2,20 +2,12 @@
 
 #include <Arduino.h>
 
-#define SIM_DATA_TYPE_ANALOG_READ      0x00
-#define SIM_DATA_TYPE_01               0x01
-#define SIM_DATA_TYPE_02               0x02
-#define SIM_DATA_TYPE_03               0x03
-#define SIM_DATA_TYPE_04               0x04
-#define SIM_DATA_TYPE_05               0x05
-#define SIM_DATA_TYPE_06               0x06
-#define SIM_DATA_TYPE_07               0x07
-#define SIM_DATA_TYPE_08               0x08
-#define SIM_DATA_TYPE_09               0x09
-#define SIM_DATA_TYPE_10               0x0A
+#define DATA_ANALOG_READ_ESP32      0x00
+#define DATA_ANALOG_READ_SIMPLE     0x01
+#define DATA_GENERIC                0x02
 
-#define SIM_DATA_TYPE  SIM_DATA_TYPE_01
-#define GENERIC_DATA
+#define SIM_DATA_TYPE  DATA_ANALOG_READ_ESP32
+
 
 uint16_t holding_registers[TOTAL_HOLDING_REGISTERS];
 
@@ -37,23 +29,7 @@ uint16_t holding_registers[TOTAL_HOLDING_REGISTERS];
 #define ELTotalCurrentVl     holding_registers[14]
 
 
-/*************************************************************/
-/* Generic data                                              */
-/*************************************************************/
-#ifdef GENERIC_DATA
-
-#define TOTAL_VALUES 72
-
-uint16_t const sines[TOTAL_VALUES] = { 140, 151, 163, 173, 184, 194, 204, 214, 223, 231, 238, 245, 251, 256, 261, 264, 266, 268, 268, 268, 266, 264, 261, 256, 251, 245, 238, 231, 223, 214, 204, 194, 184, 173, 163, 151, 140, 129, 118, 107, 97, 86, 76, 67, 58, 50, 42, 35, 29, 24, 20, 17, 14, 13, 12, 13, 14, 17, 20, 24, 29, 35, 42, 50, 58, 67, 76, 86, 97, 107, 118, 129 };
-uint16_t const cosines[TOTAL_VALUES] = { 268, 268, 266, 264, 261, 256, 251, 245, 238, 231, 223, 214, 204, 194, 184, 173, 163, 151, 140, 129, 118, 107, 97, 86, 76, 67, 58, 50, 42, 35, 29, 24, 20, 17, 14, 13, 12, 13, 14, 17, 20, 24, 29, 35, 42, 50, 58, 67, 76, 86, 97, 107, 118, 129, 140, 151, 163, 173, 184, 194, 204, 214, 223, 231, 238, 245, 251, 256, 261, 264, 266, 268 };
-
-#endif
-
-/*************************************************************/
-/* Implementing simulated data                               */
-/*************************************************************/
-
-#if SIM_DATA_TYPE == SIM_DATA_TYPE_ANALOG_READ
+#if SIM_DATA_TYPE == DATA_ANALOG_READ_ESP32
 
 static const byte pot_pins[6] = { A0, A1, A2, A3, A4, A5 };
 
@@ -85,7 +61,44 @@ void fill_sim_data() {
   holding_registers[15] = 100;
 }
 
-#elif  SIM_DATA_TYPE == SIM_DATA_TYPE_01
+#elif SIM_DATA_TYPE == DATA_ANALOG_READ_SIMPLE
+
+static const byte pot_pins[6] = { A0, A1, A2, A3, A4, A5 };
+
+void init_sim_data() {
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+  pinMode(A5, INPUT);
+}
+
+void fill_sim_data() {
+  holding_registers[0] = map(analogRead(pot_pins[0]), 0, 1023, 0, 255);
+  holding_registers[1] = map(analogRead(pot_pins[1]), 0, 1023, 0, 255);
+  holding_registers[2] = map(analogRead(pot_pins[2]), 0, 1023, 0, 255);
+  holding_registers[3] = map(analogRead(pot_pins[3]), 0, 1023, 0, 255);
+  holding_registers[4] = map(analogRead(pot_pins[4]), 0, 1023, 0, 255);
+  holding_registers[5] = map(analogRead(pot_pins[5]), 0, 1023, 0, 255);
+  holding_registers[6] = 100;
+  holding_registers[7] = 100;
+  holding_registers[8] = 1000;
+  holding_registers[9] = 1000;
+  holding_registers[10] = 0;
+  holding_registers[11] = 1;
+  holding_registers[12] = 0;
+  holding_registers[13] = 10;
+  holding_registers[14] = 0;
+  holding_registers[15] = 100;
+}
+
+#elif  SIM_DATA_TYPE == DATA_GENERIC
+
+#define TOTAL_VALUES 72
+
+uint16_t const sines[TOTAL_VALUES] = { 140, 151, 163, 173, 184, 194, 204, 214, 223, 231, 238, 245, 251, 256, 261, 264, 266, 268, 268, 268, 266, 264, 261, 256, 251, 245, 238, 231, 223, 214, 204, 194, 184, 173, 163, 151, 140, 129, 118, 107, 97, 86, 76, 67, 58, 50, 42, 35, 29, 24, 20, 17, 14, 13, 12, 13, 14, 17, 20, 24, 29, 35, 42, 50, 58, 67, 76, 86, 97, 107, 118, 129 };
+uint16_t const cosines[TOTAL_VALUES] = { 268, 268, 266, 264, 261, 256, 251, 245, 238, 231, 223, 214, 204, 194, 184, 173, 163, 151, 140, 129, 118, 107, 97, 86, 76, 67, 58, 50, 42, 35, 29, 24, 20, 17, 14, 13, 12, 13, 14, 17, 20, 24, 29, 35, 42, 50, 58, 67, 76, 86, 97, 107, 118, 129, 140, 151, 163, 173, 184, 194, 204, 214, 223, 231, 238, 245, 251, 256, 261, 264, 266, 268 };
 
 void init_sim_data() {}
 
@@ -123,24 +136,6 @@ void fill_sim_data() {
   }
 }
 
-#elif  SIM_DATA_TYPE == SIM_DATA_TYPE_02
-#  error "SIM_DATA_TYPE_02 not implemented"
-#elif  SIM_DATA_TYPE == SIM_DATA_TYPE_03
-#  error "SIM_DATA_TYPE_03 not implemented"
-#elif  SIM_DATA_TYPE == SIM_DATA_TYPE_04
-#  error "SIM_DATA_TYPE_04 not implemented"
-#elif  SIM_DATA_TYPE == SIM_DATA_TYPE_05
-#  error "SIM_DATA_TYPE_05 not implemented"
-#elif  SIM_DATA_TYPE == SIM_DATA_TYPE_06
-#  error "SIM_DATA_TYPE_06 not implemented"
-#elif  SIM_DATA_TYPE == SIM_DATA_TYPE_07
-#  error "SIM_DATA_TYPE_07 not implemented"
-#elif  SIM_DATA_TYPE == SIM_DATA_TYPE_08
-#  error "SIM_DATA_TYPE_08 not implemented"
-#elif  SIM_DATA_TYPE == SIM_DATA_TYPE_09
-#  error "SIM_DATA_TYPE_09 not implemented"
-#elif  SIM_DATA_TYPE == SIM_DATA_TYPE_10
-#  error "SIM_DATA_TYPE_10 not implemented"
 #else
 #  error "SIM_DATA_TYPE not defined"
 #endif
