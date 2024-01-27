aa8b7d4685506c5c4ded70431d19773628d1a05e
usama <mahmoodify@gmail.com>
2024-01-27 18:49:06 +0500

    impl regs filling for esp32

--- Src/ArduinoSimDevice/SimData.cpp
+++ Src/ArduinoSimDevice/SimData.cpp
@@ -32,34 +32,39 @@ uint16_t holding_registers[TOTAL_HOLDING_REGISTERS];
 
 #if SIM_DATA_TYPE == DATA_ANALOG_READ_ESP32
 
-static const byte pot_pins[6] = { A0, A1, A2, A3, A4, A5 };
+#define ADC_MAX_VOLTAGE     3.3
+#define ADC_QNT_NUMBERS     4096
+#define ADC_VAL_TO_VOLT     ADC_MAX_VOLTAGE / ADC_QNT_NUMBERS
+#define VOLTAGE_SCALING     1000
+#define SET_REGISTER(__n,__pin) {               \
+               pinMode(__pin, INPUT);           \
+               holding_registers[__n] =         \
+                   (uint16_t)(                  \
+                      analogRead(__pin) *       \
+                          ADC_VAL_TO_VOLT *     \
+                          VOLTAGE_SCALING       \
+                      );                        \
+               pinMode(__pin, INPUT_PULLUP);    \
+        }
 
-void init_sim_data() {
-  pinMode(A0, INPUT);
-  pinMode(A1, INPUT);
-  pinMode(A2, INPUT);
-  pinMode(A3, INPUT);
-  pinMode(A4, INPUT);
-  pinMode(A5, INPUT);
-}
+void init_sim_data() {}
 
 void fill_sim_data() {
-  holding_registers[0] = map(analogRead(pot_pins[0]), 0, 1023, 0, 255);
-  holding_registers[1] = map(analogRead(pot_pins[1]), 0, 1023, 0, 255);
-  holding_registers[2] = map(analogRead(pot_pins[2]), 0, 1023, 0, 255);
-  holding_registers[3] = map(analogRead(pot_pins[3]), 0, 1023, 0, 255);
-  holding_registers[4] = map(analogRead(pot_pins[4]), 0, 1023, 0, 255);
-  holding_registers[5] = map(analogRead(pot_pins[5]), 0, 1023, 0, 255);
-  holding_registers[6] = 100;
-  holding_registers[7] = 100;
-  holding_registers[8] = 1000;
-  holding_registers[9] = 1000;
-  holding_registers[10] = 0;
-  holding_registers[11] = 1;
-  holding_registers[12] = 0;
-  holding_registers[13] = 10;
-  holding_registers[14] = 0;
-  holding_registers[15] = 100;
+  SET_REGISTER(0, 36);
+  SET_REGISTER(1, 39);
+  SET_REGISTER(2, 34);
+  SET_REGISTER(3, 35);
+  SET_REGISTER(4, 32);
+  SET_REGISTER(5, 33);
+  SET_REGISTER(6, 25);
+  SET_REGISTER(7, 26);
+  SET_REGISTER(8, 27);
+  SET_REGISTER(9, 14);
+  SET_REGISTER(10, 12);
+  SET_REGISTER(11, 13);
+  SET_REGISTER(12, 15);
+  SET_REGISTER(13, 2);
+  SET_REGISTER(14, 4);
 }
 
 #elif SIM_DATA_TYPE == DATA_ANALOG_READ_SIMPLE
