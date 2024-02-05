f730e427f2b8f887df19ed7da756c156e01157b0
usama <29387130+usa-m@users.noreply.github.com>
2024-02-06 00:26:18 +0500

    cleanup

--- Src/FuelCellMonitor.WpfApp/Windows/AboutWindow.xaml.cs
+++ Src/FuelCellMonitor.WpfApp/Windows/AboutWindow.xaml.cs
@@ -1,27 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace CronBlocks.FuelCellMonitor.Windows
+namespace CronBlocks.FuelCellMonitor.Windows;
+
+public partial class AboutWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for AboutWindow.xaml
-    /// </summary>
-    public partial class AboutWindow : Window
+    public AboutWindow()
     {
-        public AboutWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
 }
